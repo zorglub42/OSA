1355c3b551721f50b2d7f1602c9c8b079d63a0e0
root <contact@zorglub42.fr>
2017-02-28 13:59:40 +0100

    headers mapping

--- ApplianceManager.php/include/Settings.ini.php
+++ ApplianceManager.php/include/Settings.ini.php
@@ -63,12 +63,20 @@
 		
 
 	#Identity forwarding headers name
+	define ("defaultHeadersName", array("userName" => "X_REMOTE_USER",
+										"firstName" => "X_OSA_FIRSTNAME",
+										"lastName" => "X_OSA_LASTNAME",
+										"entity" => "X_OSA_ENTITY",
+										"emailAddress" => "X_OSA_EMAIL",
+										"extra" => "X_OSA_EXTRA")
+	);
+/*							  
 	define("userNameHeader", "X_REMOTE_USER");
 	define("firstNameHeader", "NURS_FIRSTNAME");
 	define("lastNameHeader", "NURS_LASTNAME");
 	define("entityHeader", "NURS_ENTITY");
 	define("emailAddressHeader", "NURS_EMAIL");
-	define("extraHeader", "NURS_EXTRA");
+	define("extraHeader", "NURS_EXTRA");*/
 	
 	
 	#list pagination
