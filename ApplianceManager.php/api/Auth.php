4931acbce5212a600a966dccd55429661eb65375
jmjb0521 <contact@zorglub42.fr>
2017-03-06 21:43:05 +0100

    Restler3 + SwaggerUI

---
+++ ApplianceManager.php/api/Auth.php
@@ -0,0 +1,164 @@
+<?php
+/*--------------------------------------------------------
+ * Module Name : ApplianceManager
+ * Version : 1.0.0
+ *
+ * Software Name : OpenServicesAccess
+ * Version : 2.0
+ *
+ * Copyright (c) 2011 – 2017 Orange
+ * This software is distributed under the Apache 2 license
+ * <http://www.apache.org/licenses/LICENSE-2.0.html>
+ *
+ *--------------------------------------------------------
+ * File Name   : ApplianceManager/ApplianceManager.php/users/Login.php
+ *
+ * Created     : 2017-03
+ * Authors     : Benoit HERARD <benoit.herard(at)orange.com>
+ *
+ * Description :
+ *      Authentication API
+ * 
+ *--------------------------------------------------------
+ * History     :
+ * 1.0.0 - 2017-03-06 : Release of the file
+*/
+require_once('../include/commonHeaders.php');
+
+require_once '../objects/Error.class.php';
+require_once '../objects/AuthToken.class.php';
+require_once '../include/Constants.php';
+require_once '../include/Func.inc.php';
+require_once '../include/PDOFunc.php';
+require_once '../include/HTTPClient.php';
+require_once '../include/Settings.ini.php';
+
+class Auth{
+	/**
+	 * Logout from system 
+	 * 
+	 * Logout and unset authentication cookie
+	 * 
+	 * @url DELETE /logout  
+	 * @url GET /logout
+	 * 
+	 * @return string previously connected userName 
+	 */
+	function deleteTokensOfUserFromToken(){
+		GLOBAL $BDName;
+		GLOBAL $BDUser;
+		GLOBAL $BDPwd;
+		
+		
+		$error = new OSAError();
+		$error->setHttpStatus(200);
+		if (isset($_COOKIE[authTokenCookieName])){
+			try {
+				$db=openDB($BDName, $BDUser, $BDPwd );
+				$strSQL="";
+				$strSQL=$strSQL . "SELECT * FROM authtoken WHERE token=?";
+				
+				$stmt=$db->prepare($strSQL);
+				$stmt->execute(array($_COOKIE[authTokenCookieName]));
+				
+				$row = $stmt->fetch(PDO::FETCH_ASSOC);
+				if (!$row){
+					$error->setHttpStatus(404);
+					$error->setFunctionalLabel("Session not found");
+
+				}else{
+					$stmt=$db->prepare("DELETE FROM authtoken WHERE userName=?");
+					$stmt->execute(array($row["userName"]));
+					setcookie(authTokenCookieName, rand(1,1000000000), time()-3600,"/");
+					return $row["userName"];
+				}
+			}catch (Exception $e){
+				if ($error->getHttpStatus() != 200){
+					throw new RestException($error->getHttpStatus(),$error->getFunctionalLabel());
+				}else{
+					throw new RestException(500,$e->getMessage());
+				}
+			}
+		}else{
+			throw new RestException(400,"Session cookie not found");
+		}
+	}
+	
+	/**
+	 * Login
+	 * 
+	 * Login with user/passord and set authentication cookie
+	 * 
+	 * @param string userName User namme to log in
+	 * @param string password Password to authenticate
+	 * 
+	 * @url POST /login
+	 * 
+	 * @return AuthToken Authentication token 
+	 */
+	function generateTokenFormUserAndPass($userName, $password){
+
+			$httpClient = new HttpClient("","","","",true);
+			
+			$headers=Array("Accept: application/json");
+			$httpResponse=$httpClient->Post(osaAdminUri . "/auth/token", "", $headers, $userName,$password);
+			if ($httpResponse->getStatusCode() != 200){
+				
+				throw new RestException($httpResponse->getStatusCode(), $httpResponse->getStatusLabel() . "(backend=" . osaAdminUri . "/auth/token" . ")");
+			}
+			/*foreach ($httpResponse->getHeaders() as $key => $value){
+					header($key . ": " . $value);
+			}*/
+			$tokenObj=json_decode($httpResponse->getBody(), true);
+			if (isset($request_data["d"]) && $request_data["d"] != ""){
+				setcookie(authTokenCookieName, $tokenObj["token"],NULL,"/",$request_data["d"]);
+			}else{
+				setcookie(authTokenCookieName, $tokenObj["token"],NULL,"/");
+			}
+
+			return $tokenObj;
+	}
+	private function getAleat(){
+		return sprintf("-%010d", rand(1,1000000000)); 
+	}
+
+	/**
+	 * Generate authentication token for authenticated user
+	 * 
+	 * @url POST /token 
+	 * 
+	 * @return AuthToken Token
+	 */
+	function generate(){
+		GLOBAL $BDName;
+		GLOBAL $BDUser;
+		GLOBAL $BDPwd;
+		
+		$requestor=getRequestor($_REQUEST);
+		$token=time() . $this->getAleat() . $this->getAleat() . $this->getAleat() . $this->getAleat() ;
+		
+
+	
+		try {
+			$db=openDB($BDName, $BDUser, $BDPwd );
+			
+			$db->exec("DELETE FROM authtoken WHERE validUntil<now()");
+
+			$strSQL="";
+			$strSQL=$strSQL . "INSERT INTO authtoken (token, validUntil, userName) ";
+			$strSQL=$strSQL . "VALUES (";
+			$strSQL=$strSQL . 		"?,"; 
+			$strSQL=$strSQL . 		" date_add(now() ,interval ? minute) , ";
+			$strSQL=$strSQL . 		"?";
+			$strSQL=$strSQL . ")";
+			
+			$stmt=$db->prepare($strSQL);
+			$stmt->execute(array($token, authTokenTTL, $requestor));
+		}catch (Exception $e){
+			throw new RestException(500,$e->getMessage);
+		}
+		return Array("token" => $token);
+	
+	}
+}
+?>
---
+++ ApplianceManager.php/objects/AuthToken.class.php
@@ -0,0 +1,32 @@
+<?php
+/*--------------------------------------------------------
+ * Module Name : ApplianceManager
+ * Version : 1.0.0
+ *
+ * Software Name : OpenServicesAccess
+ * Version : 1.0
+ *
+ * Copyright (c) 2011 – 2014 Orange
+ * This software is distributed under the Apache 2 license
+ * <http://www.apache.org/licenses/LICENSE-2.0.html>
+ *
+ *--------------------------------------------------------
+ * File Name   : ApplianceManager/ApplianceManager.php/objects/Error.class.php
+ *
+ * Created     : 2012-02
+ * Authors     : Benoit HERARD <benoit.herard(at)orange.com>
+ *
+ * Description :
+ *      .../...
+ *--------------------------------------------------------
+ * History     :
+ * 1.0.0 - 2012-10-01 : Release of the file
+*/
+
+class AuthToken{
+	/**
+	 * @var string Authentication Token
+	 */
+	public $token;
+
+}
