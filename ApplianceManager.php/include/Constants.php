7a95076e67c9c180c23c58d6b62778e91e05221b
zorglub42 <zorglub42@users.noreply.github.com>
2018-07-09 09:29:06 +0200

    Update Constants.php

--- ApplianceManager.php/include/Constants.php
+++ ApplianceManager.php/include/Constants.php
@@ -67,7 +67,7 @@ define("VALID_USER_GROUP", "valid-user");
 define("ADMIN_USER", "Admin");
 define("ADMIN_SERVICE", "ApplianceManagerAdmin");
 
-define("version", "4.0-rc3");
+define("version", "4.0");
 
 
 $userProperties = array("userName", 
