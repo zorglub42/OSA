d71de31a43d424266ec79c394e259ccb0d190332
Zorglub42 <contact@zorglub42.fr>
2019-02-19 10:24:08 +0100

    4.1

--- ApplianceManager.php/include/Constants.php
+++ ApplianceManager.php/include/Constants.php
@@ -67,7 +67,7 @@ define("VALID_USER_GROUP", "valid-user");
 define("ADMIN_USER", "Admin");
 define("ADMIN_SERVICE", "ApplianceManagerAdmin");
 
-define("version", "4.1-beta");
+define("version", "4.1");
 
 
 $userProperties = array("userName", 
